321ceca802d1a4b8ee82b753ca317502873a0b17
SSen <i@ssen.name>
2019-03-21 17:19:46 +0900

    publish: 2.0.14

--- lib/utils/module/buildTypescriptDeclarations.d.ts
+++ lib/utils/module/buildTypescriptDeclarations.d.ts
@@ -2,6 +2,7 @@ import { Config, ModuleBuildOption } from '../../types';
 interface Params {
     buildOption: ModuleBuildOption;
     appDirectory: Config['appDirectory'];
+    zeroconfigDirectory: Config['zeroconfigDirectory'];
 }
-declare const _default: ({ buildOption, appDirectory }: Params) => Promise<void>;
+declare const _default: ({ buildOption, appDirectory, zeroconfigDirectory }: Params) => Promise<void>;
 export = _default;
