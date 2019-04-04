1815784e3d439f69b2a367b021fadd46724c2bba
SSen <i@ssen.name>
2019-04-04 15:32:25 +0900

    feat: Add storybook config

---
+++ lib/updateStorybookWebpackConfig.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="webpack-dev-server" />
+import { Configuration } from 'webpack';
+declare const _default: ({ config }: {
+    config: Configuration;
+}) => Configuration;
+export = _default;
