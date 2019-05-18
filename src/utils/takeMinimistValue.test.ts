7bab4bbb9cd5612fa30bbf2b967092072509984e
SSen <i@ssen.name>
2019-05-18 16:11:37 +0900

    test: Update test title

--- src/utils/takeMinimistValue.test.ts
+++ src/utils/takeMinimistValue.test.ts
@@ -3,7 +3,7 @@ import { takeMinimistEveryValues } from './takeMinimistEveryValues';
 import { takeMinimistLatestValue } from './takeMinimistLatestValue';
 
 describe('takeLatest()', () => {
-  test('get value', () => {
+  test('같은 argv가 여럿 입력되었을때 마지막 값을 가져온다', () => {
     const argv: ParsedArgs = minimist(['build', 'app', '--size-report=true', '--compress=false', '--size-report=false']);
     expect(argv._).toEqual(['build', 'app']);
     expect(takeMinimistLatestValue(argv['size-report'])).toEqual('false');
@@ -13,7 +13,7 @@ describe('takeLatest()', () => {
 });
 
 describe('takeEvery()', () => {
-  test('get value', () => {
+  test('같은 argv가 여럿 입력되었을때 모든 값을 가져온다', () => {
     const argv: ParsedArgs = minimist(['build', 'app', '--static-files', 'a b', '--static-files', 'c d']);
     expect(argv._).toEqual(['build', 'app']);
     expect(takeMinimistEveryValues(argv['static-files'])).toEqual('a b c d');
