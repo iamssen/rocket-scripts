58602cd3e7904d9ec7b6ea13f9cbbf682e8a16e3
SSen <i@ssen.name>
2019-08-09 12:09:54 +0900

    feat: copy *.d.ts files in _packages directories

--- src/runners/fsCopySourceFilter.ts
+++ src/runners/fsCopySourceFilter.ts
@@ -1,8 +1,10 @@
 import fs from 'fs-extra';
 
 export function fsCopySourceFilter(src: string): boolean {
-  if (!/\.(ts|tsx|js|jsx)$/.test(src)) {
-    if (fs.statSync(src).isFile()) console.log(src);
+  if (!/\.(ts|tsx|mjs|js|jsx)$/.test(src) || /\.d\.ts$/.test(src)) {
+    if (!process.env.JEST_WORKER_ID) {
+      if (fs.statSync(src).isFile()) console.log(src);
+    }
     return true;
   }
   return false;
