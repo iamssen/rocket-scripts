52b680aee8f16b4e7f8cdf8cd75b857aec307be0
SSen <i@ssen.name>
2020-07-21 00:16:25 +0900

    test: increase default timeout to 40000

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ const jestPreset = require('rocket-punch/jest-preset');
 
 module.exports = {
   ...jestPreset,
-  
+
+  setupFilesAfterEnv: [...jestPreset.setupFilesAfterEnv, './jest.setup.js'],
+
   collectCoverageFrom: [
     ...jestPreset.collectCoverageFrom,
     '!src/@rocket-scripts/*/commands.ts',
