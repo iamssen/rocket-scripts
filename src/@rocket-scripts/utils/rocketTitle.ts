e89cf8585ad3ca718ae56bc296e4ec18517b255b
SSen <i@ssen.name>
2020-07-29 21:09:22 +0900

    fix: circularly dependency

--- src/@rocket-scripts/utils/rocketTitle.ts
+++ src/@rocket-scripts/utils/rocketTitle.ts
@@ -1,13 +1,15 @@
 import { render } from 'cfonts';
 
-let version: string | null = null;
+//let version: string | null = null;
 
-try {
-  const packageJson = require('@rocket-scripts/web/package.json');
-  version = packageJson?.version ?? null;
-} catch {
-  version = null;
-}
+//try {
+//  const packageJson = require('@rocket-scripts/web/package.json');
+//  version = packageJson?.version ?? null;
+//} catch {
+//  version = null;
+//}
 
-export const rocketTitle: string =
-  render('ROCKET', { font: 'block' }).string + (version ? `\n ${version}` : '');
+//export const rocketTitle: string =
+//  render('ROCKET', { font: 'block' }).string + (version ? `\n ${version}` : '');
+
+export const rocketTitle: string = render('ROCKET', { font: 'block' }).string;
