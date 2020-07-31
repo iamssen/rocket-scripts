219a60d0c56f45efcb679a5648eb99c8c5fcb0da
SSen <i@ssen.name>
2020-07-31 21:59:23 +0900

    test: try fix related https://github.com/kulshekhar/ts-jest/issues/1500

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ const jestPreset = require('rocket-punch/jest-preset');
 module.exports = {
   ...jestPreset,
 
+  // https://github.com/kulshekhar/ts-jest/issues/1500
+  // https://github.com/rocket-hangar/rocket-scripts/runs/931795831?check_suite_focus=true#step:11:95
+  transformIgnorePatterns: ['\\.webpack\\.config\\.js$'],
+
   setupFilesAfterEnv: [...jestPreset.setupFilesAfterEnv, './jest.setup.js'],
 
   collectCoverageFrom: [
