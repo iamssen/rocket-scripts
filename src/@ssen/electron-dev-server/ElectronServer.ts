f55ce1ef04d640257ce7080a3f3353beb866292d
SSen <i@ssen.name>
2020-08-08 15:19:20 +0900

    feat: auto restart electron process when main update

--- src/@ssen/electron-dev-server/ElectronServer.ts
+++ src/@ssen/electron-dev-server/ElectronServer.ts
@@ -1,5 +1,7 @@
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { watch } from 'chokidar';
 import electron from 'electron';
+import { FSWatcher } from 'fs';
 
 export interface ElectronServerParams {
   dir: string;
@@ -9,23 +11,30 @@ export interface ElectronServerParams {
 
 export class ElectronServer {
   private proc: ChildProcessWithoutNullStreams | null = null;
+  private watcher: FSWatcher | null = null;
 
   constructor(private params: ElectronServerParams) {
-    this.start();
+    this.startProc();
+    this.watcher = watch(params.main).on('change', this.restart);
   }
 
-  public start = () => {
-    const { main, dir, argv } = this.params;
-    //@ts-ignore
-    this.proc = spawn(electron, [...argv, main], { cwd: dir, shell: true });
-  };
-
   public restart = () => {
-    this.close();
-    this.start();
+    this.proc?.kill();
+    this.startProc();
   };
 
   public close = () => {
     this.proc?.kill();
+    this.proc = null;
+
+    this.watcher?.close();
+    this.watcher = null;
+  };
+
+  private startProc = () => {
+    const { main, dir, argv } = this.params;
+
+    //@ts-ignore
+    this.proc = spawn(electron, [...argv, main], { cwd: dir, shell: true });
   };
 }
