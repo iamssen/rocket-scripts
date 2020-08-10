bb1c514a84e136dcd070c37d47e063712dc7b772
SSen <i@ssen.name>
2020-08-10 19:17:32 +0900

    test: increase test timeout

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const jestPreset = require('rocket-punch/jest-preset');
 module.exports = {
   ...jestPreset,
   
-  testTimeout: 70000,
+  testTimeout: 80000,
 
   // https://github.com/kulshekhar/ts-jest/issues/1500
   // https://github.com/rocket-hangar/rocket-scripts/runs/931795831?check_suite_focus=true#step:11:95
