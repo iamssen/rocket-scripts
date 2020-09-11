2aa6d73871674fb009a1ac2615917bd62a04e92f
SSen <i@ssen.name>
2020-09-12 02:56:50 +0900

    refactor: rename directory name. source -> packages

--- .huskyrc.js
+++ .huskyrc.js
@@ -12,10 +12,10 @@ module.exports = {
   hooks: {
     'pre-commit': [
       `markdown-source-import README.md --git-add`,
-      ...cp(`README.md`, `source/src/@rocket-scripts/web/README.md`),
-      ...cp(`README.md`, `source/src/@rocket-scripts/electron/README.md`),
+      ...cp(`README.md`, `packages/src/@rocket-scripts/web/README.md`),
+      ...cp(`README.md`, `packages/src/@rocket-scripts/electron/README.md`),
       `lint-staged`,
-      ...precommit(`source`),
+      ...precommit(`packages`),
     ].join(' && '),
   },
 };
