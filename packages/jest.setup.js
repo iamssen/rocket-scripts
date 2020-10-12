91e0212a133a878861bf0821223040521b1631cb
SSen <i@ssen.name>
2020-10-12 16:09:31 +0900

    fix: can't find tar file on yarn workspace

--- packages/jest.setup.js
+++ packages/jest.setup.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-
-function getModuleDirectories(from) {
-  const moduleDirectories = [];
-  const root = path.resolve('/');
-
-  let dir = from;
-  while (dir !== root) {
-    const modules = path.resolve(dir, 'node_modules');
-
-    if (fs.existsSync(modules)) {
-      moduleDirectories.push(modules);
-    }
-
-    dir = path.dirname(dir);
-  }
-
-  return moduleDirectories.reverse().join(':');
-}
-
-process.env.NODE_PATH = getModuleDirectories(__dirname);
+//const fs = require('fs');
+//const path = require('path');
+//
+//function getModuleDirectories(from) {
+//  const moduleDirectories = [];
+//  const root = path.resolve('/');
+//
+//  let dir = from;
+//  while (dir !== root) {
+//    const modules = path.resolve(dir, 'node_modules');
+//
+//    if (fs.existsSync(modules)) {
+//      moduleDirectories.push(modules);
+//    }
+//
+//    dir = path.dirname(dir);
+//  }
+//
+//  return moduleDirectories.reverse().join(':');
+//}
+//
+//process.env.NODE_PATH = getModuleDirectories(__dirname);
